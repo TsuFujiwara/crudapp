188ad31d92ab18719303734d9da40280785bf0b3
Yuya Fujiwara <y.fujiwara@tsushou.co.jp>
2019-04-03 13:41:24 +0900

    first commit

--- server.js
+++ server.js
@@ -1,73 +1,74 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import path from 'path'
-import mongoose from 'mongoose'
-import Character from './character'
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import mongoose from "mongoose";
+import Character from "./character";
+import "dotenv/config";
 
-const app = express()
-const port = process.env.PORT || 3001
-const dbUrl = process.env.MONGODB_URI
+const app = express();
+const port = process.env.PORT || 3001;
+const dbUrl = process.env.MONGODB_URI;
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose.connect(dbUrl, dbErr => {
-  if (dbErr) throw new Error(dbErr)
-  else console.log('db connected')
+  if (dbErr) throw new Error(dbErr);
+  else console.log("db connected");
 
-  app.post('/api/characters', (request, response) => {
-    const { name, age } = request.body
+  app.post("/api/characters", (request, response) => {
+    const { name, age } = request.body;
 
     new Character({
       name,
-      age,
+      age
     }).save(err => {
-      if (err) response.status(500)
+      if (err) response.status(500);
       else {
         Character.find({}, (findErr, characterArray) => {
-          if (findErr) response.status(500).send()
-          else response.status(200).send(characterArray)
-        })
+          if (findErr) response.status(500).send();
+          else response.status(200).send(characterArray);
+        });
       }
-    })
-  })
+    });
+  });
 
-  app.get('/api/characters', (request, response) => {
+  app.get("/api/characters", (request, response) => {
     Character.find({}, (err, characterArray) => {
-      if (err) response.status(500).send()
-      else response.status(200).send(characterArray)
-    })
-  })
+      if (err) response.status(500).send();
+      else response.status(200).send(characterArray);
+    });
+  });
 
-  app.put('/api/characters', (request, response) => {
-    const { id } = request.body
-    Character.findByIdAndUpdate(id, { $inc: {"age": 1} }, err => {
-      if (err) response.status(500).send()
+  app.put("/api/characters", (request, response) => {
+    const { id } = request.body;
+    Character.findByIdAndUpdate(id, { $inc: { age: 1 } }, err => {
+      if (err) response.status(500).send();
       else {
         Character.find({}, (findErr, characterArray) => {
-          if (findErr) response.status(500).send()
-          else response.status(200).send(characterArray)
-        })
+          if (findErr) response.status(500).send();
+          else response.status(200).send(characterArray);
+        });
       }
-    })
-  })
+    });
+  });
 
-  app.delete('/api/characters', (request, response) => {
-    const { id } = request.body
+  app.delete("/api/characters", (request, response) => {
+    const { id } = request.body;
     Character.findByIdAndRemove(id, err => {
-      if (err) response.status(500).send()
+      if (err) response.status(500).send();
       else {
         Character.find({}, (findErr, characterArray) => {
-          if (findErr) response.status(500).send()
-          else response.status(200).send(characterArray)
-        })
+          if (findErr) response.status(500).send();
+          else response.status(200).send(characterArray);
+        });
       }
-    })
-  })
+    });
+  });
 
   app.listen(port, err => {
-    if (err) throw new Error(err)
-    else console.log(`listening on port ${port}`)
-  })
-})
+    if (err) throw new Error(err);
+    else console.log(`listening on port ${port}`);
+  });
+});
